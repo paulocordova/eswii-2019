9ed01098665411d7bea1f298b1a274e95d380a22
Paulo Roberto Córdova <paulo.cdv@gmail.com>
2019-10-09 21:30:58 -0300

    Atualização para ajustes gerais

--- tests/tests.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('../controllers/ControllerUsuario.php');
-
-use CONTROLLERS\ControllerUsuario;
-
-$ctrlUsuario = new ControllerUsuario();
-
-$ctrlUsuario->salvarUsuario('Marcos Dias', 'dias@noites.com.br', 'dias', '45632');
