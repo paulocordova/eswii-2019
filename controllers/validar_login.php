8594afce3b20266de692e1ea42673804cc36841d
Paulo Roberto Córdova <paulo.cdv@gmail.com>
2019-10-25 22:15:58 -0300

    Atualizado com correções

---
+++ controllers/validar_login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once('Usuario/ControllerUsuario.php');
+use controllers\ControllerUsuario;
+
+if(isset($_POST['login']) && isset($_POST['senha'])){
+   $login = $_POST['login'];
+   $senha = $_POST['senha'];
+
+   $ctrlUsuario = new ControllerUsuario();
+   $usuario = $ctrlUsuario->fazerLogin();
+
+   if($usuario->logado){
+      $_SESSION['usuario'] = serialize($usuario);
+      header("Location: ../views/main.php");
+   }
+}else{
+   $_SESSION['erroLogin'] = "Você precisa fazer login para acessar o sistema";
+   header("Location: ../index.php");
+}
+
+?>
